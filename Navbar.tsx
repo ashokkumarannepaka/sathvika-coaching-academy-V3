d2d5e3c8584eb30c088b426bd1295b8f093f2479
ashokkumarannepaka <ashokkumarannepaka@gmail.com>
2026-06-02 03:23:08 +0530

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
           {/* Logo */}
           <div className="flex items-center">
            <img
-  src="Public/sathvika-logo.png"
+  src="public/sathvika-logo.png"
   alt="Sathvika Logo"
   className="h-20 w-auto object-contain"
 />
